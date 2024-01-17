f1888fb26215f6c1d874323aa9fe1053666309a2
Giani <giani.statie@heits.digital>
2024-01-17 09:53:02 +0200

    final touches

--- src/ui/PlayerName.gd
+++ src/ui/PlayerName.gd
@@ -2,7 +2,7 @@ extends LineEdit
 
 
 func _on_PlayerName_focus_entered():
-	if OS.has_feature('JavaScript'):
+	if OS.has_feature('JavaScript') and OS.get_name() in ["Android", "iOS"]:
 		text = JavaScript.eval("""
 			window.prompt('Your Name')
 		""")
